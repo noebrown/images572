e5515ed323f6fc2e651ff355b70208a1b9e596eb
Noe Brown <jalena@live.unc.edu>
2022-11-29 15:27:01 -0500

    Create seamcarving_noe.m

---
+++ seamcarving_noe.m
@@ -0,0 +1,12 @@
+%% Writeups
+% * Video URL
+% <http://www.xxx.xxx>
+%
+% * Brief description as per instructions
+% 
+
+%% Your project code
+close all;
+%% Set the following two flags to "YES!" when ready to submit.
+are_you_sure_video_url_is_correct = "";
+are_you_sure_brief_description_is_provided = "";
